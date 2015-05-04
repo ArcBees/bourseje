c870f384013fe912328cd848f00b79af77c39b16
larrymatte <matte.larry@gmail.com>
2015-05-04 11:36:07 -0400

    BJEJ-1 bunch of animations and modifications

---
+++ src/main/java/com/arcbees/bourseje/client/realtime/ui/Footer.java
@@ -0,0 +1,26 @@
+package com.arcbees.bourseje.client.realtime.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+
+public class Footer implements IsWidget {
+    interface Binder extends UiBinder<HTMLPanel, Footer> {
+    }
+
+    private static Binder binder = GWT.create(Binder.class);
+
+    private final Widget widget;
+
+    public Footer() {
+        widget = binder.createAndBindUi(this);
+    }
+
+    @Override
+    public Widget asWidget() {
+        return widget;
+    }
+}
---
+++ src/main/java/com/arcbees/bourseje/client/realtime/ui/Header.java
@@ -0,0 +1,26 @@
+package com.arcbees.bourseje.client.realtime.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Widget;
+
+
+public class Header implements IsWidget {
+    interface Binder extends UiBinder<HTMLPanel, Header> {
+    }
+
+    private static Binder binder = GWT.create(Binder.class);
+
+    private final Widget widget;
+
+    public Header() {
+        widget = binder.createAndBindUi(this);
+    }
+
+    @Override
+    public Widget asWidget() {
+        return widget;
+    }
+}
