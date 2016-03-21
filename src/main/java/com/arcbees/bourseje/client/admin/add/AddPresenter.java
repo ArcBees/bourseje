8e34c284db41d5396232fe1580b1a1c42ad8e034
jasonlemay <jason.lemay@arcbees.com>
2016-03-21 16:44:03 -0400

    Update headers

--- src/main/java/com/arcbees/bourseje/client/admin/add/AddPresenter.java
+++ src/main/java/com/arcbees/bourseje/client/admin/add/AddPresenter.java
@@ -1,12 +1,12 @@
 /**
- * Copyright 2014 ArcBees Inc.
- *
+ * Copyright 2016 ArcBees Inc.
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,14 +16,9 @@
 
 package com.arcbees.bourseje.client.admin.add;
 
-import com.arcbees.bourseje.client.AdminRestCallback;
 import com.arcbees.bourseje.client.NameTokens;
 import com.arcbees.bourseje.client.admin.AdminPresenter;
 import com.arcbees.bourseje.client.api.AdminService;
-import com.arcbees.bourseje.client.model.Candidate;
-import com.arcbees.bourseje.client.model.Candidates;
-import com.arcbees.bourseje.shared.CandidateResult;
-import com.google.gwt.resources.client.ImageResource;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.dispatch.rest.client.RestDispatch;
@@ -34,7 +29,8 @@ import com.gwtplatform.mvp.client.annotations.ProxyStandard;
 import com.gwtplatform.mvp.client.proxy.ProxyPlace;
 
 public class AddPresenter extends Presenter<AddPresenter.MyView, AddPresenter.MyProxy> {
-    interface MyView extends View {}
+    interface MyView extends View {
+    }
 
     @ProxyStandard
     @NameToken(NameTokens.ADD)
--- src/main/java/com/arcbees/bourseje/client/admin/add/AddView.java
+++ src/main/java/com/arcbees/bourseje/client/admin/add/AddView.java
@@ -1,12 +1,12 @@
 /**
- * Copyright 2014 ArcBees Inc.
- *
+ * Copyright 2016 ArcBees Inc.
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * <p/>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,13 +18,7 @@ package com.arcbees.bourseje.client.admin.add;
 
 import javax.inject.Inject;
 
-import com.arcbees.bourseje.client.admin.winner.WinnerPresenter;
-import com.google.gwt.dom.client.HeadingElement;
-import com.google.gwt.dom.client.ParagraphElement;
-import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
 import com.gwtplatform.mvp.client.ViewImpl;
 
